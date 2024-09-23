a5afab0c3d3f29f54bc8d4b8bac90b7e315b4e85
Skyran C2-API <ranc2api@gmail.com>
2024-09-23 18:32:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('lihat-layanan-btn').addEventListener('click', function() {
+    const servicesSection = document.getElementById('services-section');
+    servicesSection.scrollIntoView({ behavior: 'smooth' });
+});
