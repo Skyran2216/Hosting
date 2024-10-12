6753c44151bb98097d30d66650ea806019e8cb4d
Skyran C2-API <ranc2api@gmail.com>
2024-10-12 17:00:08 +0700

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('lihat-layanan-btn').addEventListener('click', function() {
-    const servicesSection = document.getElementById('services-section');
-    servicesSection.scrollIntoView({ behavior: 'smooth' });
-});
